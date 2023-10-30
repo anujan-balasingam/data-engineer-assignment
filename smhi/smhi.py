aea395761be0fd4c1cf15d0bff64d821399a6f30
Anujan Balasingam <anujan.balasingam@gmail.com>
2023-10-30 21:36:30 +0100

    first solution

--- smhi/smhi.py
+++ smhi/smhi.py
@@ -6,12 +6,13 @@ class SmhiParser:
     """
     Class to handle communication with and extract data from the SMHI Open API.
     """
-    BASE_URL = "https://opendata-download-metobs.smhi.se/api/version/latest/parameter/{}/station/97280/period/latest-day/data"
+    BASE_URL = "https://opendata-download-metobs.smhi.se/api"
+
     def __init__(self, suffix=".json"):
         self.suffix = suffix
 
-    def _make_request(self, param="2", path=""):
-        r = requests.get(self.BASE_URL.format(param)+self.suffix)
+    def _make_request(self, path=""):
+        r = requests.get(self.BASE_URL+path+self.suffix)
         return r
 
     def check_connection(self):
@@ -31,26 +32,25 @@ def main():
         p = SmhiParser()
         for i in range(1,41):
             param = str(i)
-            response = p._make_request(param)
+            path = f"/version/latest/parameter/{i}"
+            response = p._make_request(path)
             data = response.json()
     
             title = data["title"].split(":")[0]
             summary = "(" + data["summary"] + ")"
-            print(param+",", title, summary)
+            print(str(i)+",", title, summary)
 
     if args.temperatures:
-        url = "https://opendata-download-metobs.smhi.se/api/version/latest/parameter/{}/period/latest-day/data"
-        print("hämta temps")
+        path = ""
         p = SmhiParser()
-        param = "2"
-        response = p.check_connection()
-        print(response)
-        # response = p._make_request(param)
-        # data = response.json()
-        # # Writing to sample.json
-        # with open("sample.json", "w") as outfile:
-        #     obj = json.dumps(data, indent=2, ensure_ascii=False)
-        #     outfile.write(obj)
+        # response = p.check_connection()
+        # print(response)
+        response = p._make_request(path)
+        data = response.json()
+        # Writing to sample.json
+        with open("sample.json", "w") as outfile:
+            obj = json.dumps(data, indent=2, ensure_ascii=False)
+            outfile.write(obj)
 
 
 if __name__ == "__main__":
