1417a5b5a2afc50997b6e70b7cf57a1bd96f2809
Anujan Balasingam <anujan.balasingam@gmail.com>
2023-10-31 17:05:03 +0100

    2nd solution

--- smhi/smhi.py
+++ smhi/smhi.py
@@ -1,6 +1,8 @@
 import argparse
 import requests
 import json
+from datetime import date
+from datetime import timedelta
 
 class SmhiParser:
     """
@@ -15,8 +17,8 @@ class SmhiParser:
         r = requests.get(self.BASE_URL+path+self.suffix)
         return r
 
-    def check_connection(self):
-        r = self._make_request()
+    def check_connection(self, path=""):
+        r = self._make_request(path)
         return r.status_code
 
 
@@ -29,28 +31,51 @@ def main():
 
     args = parser.parse_args()
     if args.parameters:
-        p = SmhiParser()
+        parser = SmhiParser()
         for i in range(1,41):
             param = str(i)
-            path = f"/version/latest/parameter/{i}"
-            response = p._make_request(path)
+            path = f"/version/latest/parameter/{param}"
+            response = parser._make_request(path)
             data = response.json()
     
             title = data["title"].split(":")[0]
-            summary = "(" + data["summary"] + ")"
-            print(str(i)+",", title, summary)
+            summary = "".join(["(", data["summary"], ")"])
+            print(param+",", title, summary)
 
     if args.temperatures:
-        path = ""
-        p = SmhiParser()
-        # response = p.check_connection()
-        # print(response)
-        response = p._make_request(path)
-        data = response.json()
-        # Writing to sample.json
-        with open("sample.json", "w") as outfile:
-            obj = json.dumps(data, indent=2, ensure_ascii=False)
-            outfile.write(obj)
+        parser = SmhiParser()
+        largest_temp = 0
+        lowest_temp = 0
+        larg_station = ""
+        low_station = ""
+        response = parser._make_request("/version/latest/parameter/2")
+        stations_dict = response.json()
+        stations = [s["key"] for s in stations_dict["station"]]
+
+        for s in stations:
+            response =  parser._make_request(f"/version/latest/parameter/2/station/{s}/period/latest-day/data")
+            # throws 404 Client Error for stations that do not have data for period=latest-day
+            if response.status_code != 200:
+                continue
+            data = response.json()
+            if data["value"]:
+                temp = float(data["value"][0]["value"])
+                station = data["station"]["name"]
+                if temp > largest_temp:
+                    largest_temp = temp
+                    larg_station =  station
+                if temp < lowest_temp:
+                    lowest_temp = temp
+                    low_station =  station
+        
+        print("Highest temperature:", larg_station, largest_temp, "degrees")
+        print("Lowest temperature:", low_station, lowest_temp, "degrees")
+        # print("Station", data["station"]["name"], "Temperature", data["value"][0]["value"], "degrees", "Quality", data["value"][0]["quality"])
+
+        # for debugging
+        # obj = json.dumps(data, indent=2, ensure_ascii=False)
+        # outfile.write(obj)
+
 
 
 if __name__ == "__main__":
