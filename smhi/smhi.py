1b0180e797987fb34e3579560ad76cd16ba024f2
Anujan Balasingam <anujan.balasingam@gmail.com>
2023-11-01 21:35:24 +0100

    refactor parser

--- smhi/smhi.py
+++ smhi/smhi.py
@@ -13,14 +13,20 @@ class SmhiParser:
     def __init__(self, suffix=".json"):
         self.suffix = suffix
 
-    def _make_request(self, path=""):
-        r = requests.get(self.BASE_URL+path+self.suffix)
+    def _make_request(self, session: requests.Session,  path=""):
+        r = session.get(self.BASE_URL+path+self.suffix)
         return r
 
-    def check_connection(self, path=""):
-        r = self._make_request(path)
+    def check_connection(self, session: requests.Session, path=""):
+        r = self._make_request(session, path)
         return r.status_code
 
+
+# def parse_args(args):
+#     parser = argparse.ArgumentParser(...)
+#     parser.add_argument...
+#     # ...Create your parser as you like...
+#     return parser.parse_args(args)
 
 def main():
     parser = argparse.ArgumentParser(
@@ -34,8 +40,8 @@ def main():
         parser = SmhiParser()
         for i in range(1,41):
             param = str(i)
-            path = f"/version/latest/parameter/{param}"
-            response = parser._make_request(path)
+            with requests.Session() as session:
+               response = parser._make_request(session, f"/version/latest/parameter/{param}")
             data = response.json()
     
             title = data["title"].split(":")[0]
@@ -48,33 +54,34 @@ def main():
         lowest_temp = 0
         larg_station = ""
         low_station = ""
-        response = parser._make_request("/version/latest/parameter/2")
+        # missing_stations = []
+        with requests.Session() as session:
+            response = parser._make_request(session, "/version/latest/parameter/2")
         stations_dict = response.json()
         stations = [s["key"] for s in stations_dict["station"]]
 
-        for s in stations:
-            response =  parser._make_request(f"/version/latest/parameter/2/station/{s}/period/latest-day/data")
-            # throws 404 Client Error for stations that do not have data for period=latest-day
-            if response.status_code != 200:
-                continue
-            data = response.json()
-            if data["value"]:
-                # temp = float(data["value"][0]["value"])
-                # station = data["station"]["name"]
-                if float(data["value"][0]["value"]) > largest_temp:
-                    largest_temp = float(data["value"][0]["value"])
-                    larg_station =  data["station"]["name"]
-                if float(data["value"][0]["value"]) < lowest_temp:
-                    lowest_temp = float(data["value"][0]["value"])
-                    low_station =  data["station"]["name"]
+        with requests.Session() as session:
+            for i, s in enumerate(stations):
+                response =  parser._make_request(session,f"/version/latest/parameter/2/station/{s}/period/latest-day/data")
+                # throws 404 Client Error for stations that do not have data for period=latest-day)
+                if response.status_code != 200:
+                    # missing_stations.append(s)
+                    continue
+                data = response.json()
+                if data["value"]:
+                    temp = float(data["value"][0]["value"])
+                    station = data["station"]["name"]
+                    if temp > largest_temp:
+                        largest_temp = temp
+                        larg_station =  station
+                    if temp < lowest_temp:
+                        lowest_temp = temp
+                        low_station =  station
         
         print("Highest temperature:", larg_station, largest_temp, "degrees")
         print("Lowest temperature:", low_station, lowest_temp, "degrees")
-        # print("Station", data["station"]["name"], "Temperature", data["value"][0]["value"], "degrees", "Quality", data["value"][0]["quality"])
+        # print("The number of stations missing", len(missing_stations))
 
-        # for debugging
-        # obj = json.dumps(data, indent=2, ensure_ascii=False)
-        # outfile.write(obj)
 
 
 
--- tests/test_smhi.py
+++ tests/test_smhi.py
@@ -1,6 +1,16 @@
 from smhi.smhi import SmhiParser
-
+import requests
+import json
 
 def test_check_connection():
     parser = SmhiParser()
-    assert 200 == parser.check_connection()
+    with requests.Session() as session:
+        assert 200 == parser.check_connection(session)
+
+def test_make_request():
+    parser = SmhiParser()
+    with requests.Session() as session:
+        response = parser._make_request(session)
+        data = response.json()
+        assert True == isinstance(data, dict)
+
