585b6d14bb4d8fa00920fef76f4985fca84f41f1
anujan-balasingam <anujan.balasingam@gmail.com>
2023-11-02 10:26:23 +0100

    Ready to present (#1)
    *ready to present

--- smhi/smhi.py
+++ smhi/smhi.py
@@ -1,31 +1,91 @@
 import argparse
 import requests
+import sys
 
 
 class SmhiParser:
     """
     Class to handle communication with and extract data from the SMHI Open API.
     """
+
     BASE_URL = "https://opendata-download-metobs.smhi.se/api"
 
     def __init__(self, suffix=".json"):
         self.suffix = suffix
 
-    def _make_request(self, path=""):
-        r = requests.get(self.BASE_URL+self.suffix)
+    def _make_request(self, session: requests.Session, path=""):
+        r = session.get(self.BASE_URL + path + self.suffix)
         return r
 
-    def check_connection(self):
-        r = self._make_request()
+    def check_connection(self, session: requests.Session, path=""):
+        r = self._make_request(session, path)
         return r.status_code
 
 
-def main():
+def parse_args(args):
     parser = argparse.ArgumentParser(
         description="""Script to extract data from SMHI's Open API"""
     )
-    parser.add_argument("--parameters", action="store_true", help="List SMHI API parameters")
-    #args = parser.parse_args()
+    parser.add_argument(
+        "--parameters", action="store_true", help="List SMHI API parameters"
+    )
+    parser.add_argument(
+        "--temperatures",
+        action="store_true",
+        help="List SMHI API temperatures for stations",
+    )
+    return parser.parse_args(args)
+
+
+def main():
+    args = parse_args(sys.argv[1:])
+    if args.parameters:
+        parser = SmhiParser()
+        for i in range(1, 41):
+            param = str(i)
+            with requests.Session() as session:
+                response = parser._make_request(
+                    session, f"/version/latest/parameter/{param}"
+                )
+            data = response.json()
+            title = data["title"].split(":")[0]
+            summary = "".join(["(", data["summary"], ")"])
+            print(param + ",", title, summary)
+
+    if args.temperatures:
+        parser = SmhiParser()
+        largest_temp = 0
+        lowest_temp = 0
+        largest_station = ""
+        lowest_station = ""
+
+        with requests.Session() as session:
+            response = parser._make_request(session, "/version/latest/parameter/2")
+        stations_dict = response.json()
+        stations = [s["key"] for s in stations_dict["station"]]
+
+        with requests.Session() as session:
+            for i, s in enumerate(stations):
+                response = parser._make_request(
+                    session,
+                    f"/version/latest/parameter/2/station/{s}/period/latest-day/data",
+                )
+                # 404 Client Error for stations without period=latest-day
+                if response.status_code != 200:
+                    continue
+                data = response.json()
+                if data["value"]:
+                    temp = float(data["value"][0]["value"])
+                    station = data["station"]["name"]
+                    if temp > largest_temp:
+                        largest_temp = temp
+                        largest_station = station
+                    if temp < lowest_temp:
+                        lowest_temp = temp
+                        lowest_station = station
+
+        print("Highest temperature:", largest_station, largest_temp, "degrees")
+        print("Lowest temperature:", lowest_station, lowest_temp, "degrees")
 
 
 if __name__ == "__main__":
--- tests/test_smhi.py
+++ tests/test_smhi.py
@@ -1,6 +1,32 @@
 from smhi.smhi import SmhiParser
+from smhi.smhi import parse_args
+import requests
+import json
 
 
 def test_check_connection():
+    """
+    Test the connection to SMHI Open API function.
+    """
     parser = SmhiParser()
-    assert 200 == parser.check_connection()
+    with requests.Session() as session:
+        assert 200 == parser.check_connection(session)
+
+
+def test_make_request():
+    """
+    Test the make_request function to ensure it returns a json and subsequently a dictionary for the BASE url.
+    """
+    parser = SmhiParser()
+    with requests.Session() as session:
+        response = parser._make_request(session)
+        data = response.json()
+        assert True == isinstance(data, dict)
+
+
+def test_parse_args():
+    """
+    Test the parse_args function to ensure it returns the expected dictionary.
+    """
+    args = parse_args(["--temperatures"])
+    assert True == args.temperatures
